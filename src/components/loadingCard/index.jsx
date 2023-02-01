634a6d3c63847165d44e2a9d4d603adc5392794f
Luiz Gustavo <gustavo120wa@gmail.com>
2023-02-01 14:55:38 -0300

    correção no className

--- src/components/loadingCard/index.jsx
+++ src/components/loadingCard/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 export function LoadingCard(){
     return (
         <div className="loader">
-            <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+            <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
         </div>
     )
 }
